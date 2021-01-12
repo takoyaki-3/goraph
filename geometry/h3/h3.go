aab3638196f0960c2ea284df6be6b0c3b64c9fc3
takoyaki-3 <mail@takoyaki3.com>
2021-01-12 10:24:00 +0900

    FindR関数を追加

--- geometry/h3/h3.go
+++ geometry/h3/h3.go
@@ -47,3 +47,26 @@ func Find(g goraph.Graph,h3indexes map[h3.H3Index][]int64,latlon goraph.LatLon,r
 
 	return min_node
 }
+
+func FindR(g goraph.Graph,h3indexes map[h3.H3Index][]int64,latlon goraph.LatLon,resolution int,r float64)[]int64{
+	h3index := h3.FromGeo(h3.GeoCoord{latlon.Lat,latlon.Lon}, resolution)
+
+	hexes,_ := h3.HexRing(h3index,1)
+	hexes = append(hexes,h3index)
+
+	nodes := []int64{}
+
+	for _,v := range hexes{
+		if _,ok := h3indexes[v];!ok{
+			continue
+		}
+		for _,v := range h3indexes[v]{
+			d := geometry.HubenyDistance(g.LatLons[v],latlon)
+			if r > d {
+				nodes = append(nodes, v)
+			}
+		}
+	}
+
+	return nodes
+}
