5c16a3cf65766d953f52fd0425941dd0e895f0d2
takoyaki-3 <mail@takoyaki3.com>
2021-10-27 10:59:10 +0900

    同名の関数を追加

--- pkg/priority_queue/priority_queue.go
+++ pkg/priority_queue/priority_queue.go
@@ -60,3 +60,39 @@ func (s *MinSet) Get_min() int64 {
 func (s *MinSet) Len() int64 {
 	return s.n
 }
+
+func (s *MinSet) AddVal(key int64, val float64) {
+	var t costque
+	t.key = key
+	t.num = val
+	s.n++
+	s.que[int(val)%s.C] = append(s.que[int(val)%s.C], t)
+}
+
+func (s *MinSet) GetMin() int64 {
+	for s.n > 0 {
+		i := s.last_num % s.C
+		if len(s.que[i]) == 0 {
+			s.last_num++
+			continue
+		}
+		minI := 0
+		for j := 1; j < len(s.que[i]); j++ {
+			if s.que[i][j].num < s.que[i][minI].num {
+				minI = j
+			}
+		}
+		if int(s.que[i][minI].num) != int(s.last_num) {
+			s.last_num++
+			continue
+		}
+
+		ans := s.que[i][minI].key
+		s.que[i][minI] = s.que[i][0]
+		s.que[i] = s.que[i][1:]
+
+		s.n--
+		return ans
+	}
+	return -1
+}
